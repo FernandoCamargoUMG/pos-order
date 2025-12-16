7fc7c4ea14127f0b870160ab3bb7072e0237d252
Fernando Camargo <fcamargoa@miumg.edu.gt>
2025-12-16 14:45:33 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pos-order',
+  webDir: 'www'
+};
+
+export default config;
